a6f8d983d023c1589c2f3f85b7d3fd465ae134a0
Josh Duffy <jduffy@mines.edu>
2013-01-06 09:19:52 -0700

    edit user path requires signed-in user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,13 @@
 class SessionsController < ApplicationController
-	def new
+  	
+  def new
 	end
 
 	def create
     user = User.find_by_email(params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       sign_in user
-      redirect_to user
+      redirect_to user, :notice => 'Logged In!'
     else
       flash.now[:error] = 'Invalid email/password combination'
       render 'new'
@@ -15,6 +16,6 @@ class SessionsController < ApplicationController
 
 	def destroy
     sign_out
-    redirect_to root_url
+    redirect_to root_url, :notice => 'Logged Out!'
   end
 end
